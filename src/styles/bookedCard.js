92a121dbdd6fdafa533ee3c4872b48957ded279b
Alisa <alisa@jonnix.co>
2021-12-06 19:25:14 +0200

    edited booked card styles

---
+++ src/styles/bookedCard.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const StyledCard = styled.div`
+    border-top: 3px solid ${props => props.bordercolor};
+    min-width: 230px;
+    max-width: 260px;
+    padding: 10px;
+    margin: 10px;
+    cursor: pointer;
+    background: #373737;
+    color: #656565;
+    display: flex;
+    flex-direction: column;
+    font-weight: bold;
+`;
